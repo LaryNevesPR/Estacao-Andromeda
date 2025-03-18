628c631c85dd6c9ab69c2d0cd0dafc62b7de6a04
Lary <89530982+LaryNevesPR@users.noreply.github.com>
2025-03-18 04:53:20 -0300

    Prototype

---
+++ Content.Shared/_Andromeda/Ritual/Prototypes/RitualPrototype.cs
@@ -0,0 +1,23 @@
+﻿using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype.Array;
+
+
+namespace Content.Shared._Andromeda.Ritual.Prototypes;
+
+
+/// <summary>
+/// This is a prototype for...
+/// </summary>
+[Prototype()]
+public sealed class RitualPrototype : IPrototype
+{
+    /// <inheritdoc/>
+    [IdDataField]
+    public string ID { get; } = default!;
+
+
+    /// <inheritdoc/>
+    [NeverPushInheritance]
+    [AbstractDataField]
+    public bool Abstract { get; }
+}
