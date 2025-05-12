df11059fc194cbd5c748788fa4732bd67eb90677
Lary <89530982+LaryNevesPR@users.noreply.github.com>
2025-05-12 07:42:34 -0300

    Revert "Teste"
    This reverts commit dbf53521b6ef5118e4a0ca1b9be7c9c988c47381.

--- Content.Shared/Slippery/SlipperySystem.cs
+++ Content.Shared/Slippery/SlipperySystem.cs
@@ -74,7 +74,7 @@ public sealed class SlipperySystem : EntitySystem
                 && _statusEffects.CanApplyEffect(toSlip, "Stun"); //Should be KnockedDown instead?
     }
 
-    public void TrySlip(EntityUid uid, SlipperyComponent component, EntityUid other)
+    private void TrySlip(EntityUid uid, SlipperyComponent component, EntityUid other)
     {
         if (HasComp<KnockedDownComponent>(other) && !component.SuperSlippery)
             return;
