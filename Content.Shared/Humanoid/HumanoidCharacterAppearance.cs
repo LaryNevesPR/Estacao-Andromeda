62543691e90c86be584ae76219e4c688c9b45075
Crono209ggg <crono209gg@gmail.com>
2025-05-13 12:41:23 -0300

    adiciona a raça predador e xeno

--- Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
+++ Content.Shared/Humanoid/HumanoidCharacterAppearance.cs
@@ -108,6 +108,7 @@ public sealed partial class HumanoidCharacterAppearance : ICharacterAppearance, 
             HumanoidSkinColor.VoxFeathers => Humanoid.SkinColor.ClosestVoxColor(speciesPrototype.DefaultSkinTone),
             HumanoidSkinColor.AnimalFur => Humanoid.SkinColor.ClosestAnimalFurColor(speciesPrototype.DefaultSkinTone), // Einstein Engines - Tajaran
             HumanoidSkinColor.TintedHuesSkin => Humanoid.SkinColor.TintedHuesSkin(speciesPrototype.DefaultSkinTone, speciesPrototype.DefaultSkinTone),
+            HumanoidSkinColor.None => Color.Transparent, // Sunrise-edit - Predador
             _ => Humanoid.SkinColor.ValidHumanSkinTone,
         };
 
@@ -179,6 +180,9 @@ public sealed partial class HumanoidCharacterAppearance : ICharacterAppearance, 
                 break;
             case HumanoidSkinColor.AnimalFur: // Einstein Engines - Tajaran
                 newSkinColor = Humanoid.SkinColor.ProportionalAnimalFurColor(newSkinColor);
+                break;
+            case HumanoidSkinColor.None: // Sunrise-edit - Predador/Xeno
+                newSkinColor = Color.Transparent;
                 break;
         }
 
--- Content.Shared/Humanoid/SkinColor.cs
+++ Content.Shared/Humanoid/SkinColor.cs
@@ -344,4 +344,5 @@ public enum HumanoidSkinColor : byte
     TintedHues, //This gives a color tint to a humanoid's skin (10% saturation with full hue range).
     TintedHuesSkin, // DeltaV - Default TintedHues assumes the texture will have the proper skin color, but moths dont
     AnimalFur, // Einstein Engines - limits coloration to more or less what earthen animals might have
+    None, // No custom colors. Sunrise-edit
 }
